03e26975909e28914f4b7fd495a7b68a74d4d963
Christopher Chang <chrchang523@gmail.com>
2014-04-02 15:44:17 +0800

    add midp to binomial demo

--- binom_test.c
+++ binom_test.c
@@ -3,17 +3,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 double binom_2sided(uint32_t succ, uint32_t obs, double rate, uint32_t midp);
 double binom_1sided(int32_t succ, int32_t obs, double rate);
 
 int main(int argc, char** argv) {
+  uint32_t midp = 0;
   FILE* test_file;
   int32_t succ;
   int32_t obs;
   double rate;
   double p_value;
   char name[100];
+  if (!strcmp(argv[argc - 1], "midp")) {
+    midp = 1;
+    argc--;
+  }
   if ((argc == 4) || (argc == 5)) {
     succ = atoi(argv[1]);
     obs = atoi(argv[2]);
@@ -32,10 +38,14 @@ int main(int argc, char** argv) {
       return 3;
     }
     if (argc == 4) {
-      p_value = binom_2sided(succ, obs, rate, 0);
+      p_value = binom_2sided(succ, obs, rate, midp);
     } else {
       if (argv[4][1] || ((argv[4][0] != '+') && (argv[4][0] != '-'))) {
 	printf("Error: Invalid alternative hypothesis ('+' = more successes, '-' = fewer)\n");
+        return 3;
+      }
+      if (midp) {
+        printf("Error: 1-sided demo does not currently support mid-p adjustment.\n");
         return 3;
       }
       if (argv[4][0] == '+') {
@@ -50,7 +60,7 @@ int main(int argc, char** argv) {
     printf(
 "Binomial test    https://github.com/chrchang/stats\n"
 "(C) 2013 Christopher Chang, GNU General Public License version 3\n\n"
-"  binom_test [success ct] [total obs ct] [expected succ rate] <+ | ->\n"
+"  binom_test [success ct] [total obs ct] [expected succ rate] <+ | - | midp>\n"
 "  binom_test [filename]\n\n"
 "If a filename is provided, each line of the file is expected to contain an ID\n"
 "in the first column, and then 3 values (in succ-obs-rate order).\n");
@@ -63,7 +73,7 @@ int main(int argc, char** argv) {
   }
   while (!feof(test_file)) {
     fscanf(test_file, "%s %d %d %lg\n", name, &succ, &obs, &rate);
-    p_value = binom_2sided(succ, obs, rate, 0);
+    p_value = binom_2sided(succ, obs, rate, midp);
     printf("P-value for %s: %g\n", name, p_value);
   }
   fclose(test_file);
