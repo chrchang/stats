4f4db797d1b8dfbd0b44b3493dbac4f252801f36
Christopher Chang <chrchang523@gmail.com>
2026-05-04 09:28:11 -0700

    greater

--- Python/tests/test_exact.py
+++ Python/tests/test_exact.py
@@ -35,11 +35,28 @@ scipy_fisher22_cases = [
     ([[200, 7], [8, 300]], "less", 1),
     ([[28, 21], [6, 1957]], "less", 1),
     ([[190, 800], [200, 900]], "less", 0.7416227010368963),
+    ([[100, 2], [1000, 5]], "greater", 0.979790445314723),
+    ([[2, 7], [8, 2]], "greater", 0.9990149169715733),
+    ([[5, 1], [10, 10]], "greater", 0.1652173913043478),
+    ([[5, 15], [20, 20]], "greater", 0.9849086665340765),
+    ([[5, 16], [16, 25]], "greater", 0.9330176609214881),
+    ([[10, 5], [10, 1]], "greater", 0.9782608695652174),
+    ([[10, 5], [10, 0]], "greater", 1),
+    ([[5, 0], [1, 4]], "greater", 0.02380952380952382),
+    ([[0, 5], [1, 4]], "greater", 1),
+    ([[5, 1], [0, 4]], "greater", 0.0238095238095238),
+    ([[0, 1], [3, 2]], "greater", 1),
+    ([[200, 7], [8, 300]], "greater", 2.005657880388915e-122),
+    ([[28, 21], [6, 1957]], "greater", 5.728437460831983e-44),
+    ([[190, 800], [200, 900]], "greater", 0.2959825901308897),
     ]
 
 def test_fisher():
+    # 1e-13 is close to a thousand ULPs; good implementations should agree to
+    # that relative tolerance for small cases (such as the scipy cases where no
+    # table sum > 3000).
     for test_case in scipy_fisher22_cases:
         pval = exact_tests.fisher(test_case[0], alternative=test_case[1])
-        assert pval == pytest.approx(test_case[2])
+        assert pval == pytest.approx(test_case[2], rel=1e-13, abs=0)
     pval = exact_tests.fisher([[4, 0], [0, 4]], midp=True)
-    assert pval == pytest.approx(1 / 70)
+    assert pval == pytest.approx(1 / 70, rel=1e-13, abs=0)
